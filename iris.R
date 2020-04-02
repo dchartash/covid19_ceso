4880cce662edc49a5bb74c9169281828a76943f2
dchartas@othnielos <dchartas@othnielos>
2020-04-01 21:29:56 -0400

    Creating correct iris graphic

--- iris.R
+++ iris.R
@@ -1,6 +1,6 @@
 require('ggplot2')
 
-g<-ggplot(iris) + geom_point(aes(x=Petal.Length,y=Petal.Width,fill=Species))
-png("gfx/iris.png",width=10,height=10)
+g<-ggplot(iris) + geom_point(aes(x=Petal.Length,y=Petal.Width,color=Species))
+png("gfx/iris.png",width=10,height=10,units="in",res=400)
 plot(g)
 dev.off()
