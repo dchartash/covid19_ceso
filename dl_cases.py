f4d52f1acfdf3713be66182acd37737fd88d8972
dchartas@othnielos <dchartas@othnielos>
2020-04-03 12:30:37 -0400

    Update Fri 03 Apr 2020 12:30:49 PM EDT

--- dl_cases.py
+++ dl_cases.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 data_uri = "https://data.ontario.ca/dataset/confirmed-positive-cases-of-covid-19-in-ontario"
 r = requests.get(data_uri)
 r_soup = BeautifulSoup(r.content,'lxml-html')
-href = r_soup.find("a",{"class":"resource-url-analytics btn btn-primary"})
+href = r_soup.find("a",{"class":"resource-url-analytics btn btn-primary dataset-download-link"})
 
 r = requests.get(href['href'])
 f = open("data/conposcovidloc.csv","w")
